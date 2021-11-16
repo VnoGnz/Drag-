013f94d6b05aa38cde94eee311580c641ebd5692
VnoGnz <94194558+VnoGnz@users.noreply.github.com>
2021-11-17 06:06:15 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys,shutil
+
+runtah=["vnognz/__pycache__","modul/__pycache__","wibu/__pycache__","vnognz/ngewe/__pycache__"]
+
+if sys.version[0]!="3":
+	exit(" ! harap gunakan python3")
+
+from saya_gans import awokawokawok
+try: [shutil.rmtree(x) for x in runtah]
+except: pass
+awokawokawok()
